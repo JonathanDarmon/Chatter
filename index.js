b474b00f6b2c49cd3067f0030fe8d8d385caaaf8
Jonathan <darmon101@gmail.com>
2019-08-27 11:25:38 +0300

    v0.1

---
+++ index.js
@@ -0,0 +1,50 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// main page 
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// chat rooms
+
+app.get("/javascript", (req, res) => {
+  res.sendFile(__dirname + "/public/javascript.html");
+});
+
+app.get("/swift", (req, res) => {
+  res.sendFile(__dirname + "/public/swift.html");
+});
+
+app.get("/css", (req, res) => {
+  res.sendFile(__dirname + "/public/css.html");
+});
+
+// tech namespace
+const tech = io.of('/tech');
+
+
+tech.on("connection", (socket) => {
+  socket.on('join', (data) => {
+    socket.join(data.room);
+    tech.in(data.room).emit('message', `New user joined ${data.room} room!`)
+
+  });
+
+  socket.on("message", (data) => {
+    tech.in(data.room).emit("message", data.msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    
+    tech.emit('message', 'user disconnected');
+  });
+});
